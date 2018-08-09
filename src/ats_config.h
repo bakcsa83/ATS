71580efebc546046f25f943c53d3bbf84ad11289
unknown <zoltan@bakcsa.hu>
2018-08-09 22:50:45 +0200

    Added license header to source files

--- src/ats_config.h
+++ src/ats_config.h
@@ -1,9 +1,20 @@
 /*
- * ats_config.h
- *
- *  Created on: 12 Jun 2018
- *      Author: bakcsa
- */
+Copyright 2018 Zoltan Bakcsa (zoltan@bakcsa.hu)
+
+ This file is part of ATS (ARM Task Scheduler).
+
+ATS (ARM Task Scheduler) is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+ATS (ARM Task Scheduler) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+*/
 
 #ifndef ATS_CONFIG_H_
 #define ATS_CONFIG_H_
--- test/test.c
+++ test/test.c
@@ -1,9 +1,20 @@
 /*
- * test.c
- *
- *  Created on: 12 Jun 2018
- *      Author: bakcsa
- */
+Copyright 2018 Zoltan Bakcsa (zoltan@bakcsa.hu)
+
+ This file is part of ATS (ARM Task Scheduler).
+
+ATS (ARM Task Scheduler) is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+ATS (ARM Task Scheduler) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0-standalone.html>.
+*/
 
 #include <string.h>
 #include <stdio.h>
@@ -16,4 +27,5 @@ typedef struct my_worker{
 
 int main(void) {
 	printf("OK");
+	//TODO: to be implemented
 }
